4ffecd3af8265d6e465297629a1057313cde8fdf
IbraDirir <khaliildirir@gmail.com>
2015-09-26 18:01:04 +0300

    if elif else statement

--- learn.py
+++ learn.py
@@ -1,2 +1,10 @@
-print("hello world")
+name = "tom"
+if name is "Bucky":
+    print("hey bucky")
+elif name is "frank":
+    print("hey frank")
+elif name is "lucy":
+    print("hey lucy")
+else:
+    print("please sign  up for the site!")
 
