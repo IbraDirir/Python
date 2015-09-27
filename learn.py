03ed81e1384c2e3648bd3d3b1fd4fe4eb76e3899
IbraDirir <khaliildirir@gmail.com>
2015-09-27 12:30:21 +0300

    downloading files  from the internet

--- learn.py
+++ learn.py
@@ -1,9 +1,13 @@
-fw = open('sample.txt', 'w')
-fw.write('writing some stuff in my text file\n')
-fw.write('watching tutorials')
-fw.close()
-
-fr = open('sample.txt', 'r')
-text = fr.read()
-print(text)
-fr.close()
+from urllib import request
+yahoo_url =  "http://www.canbike.org/information-technology/yahoo-finance-key-statistics-download-to-a-csv-file.html"
+def download_stock_data(csv_url):
+     response = request.urlopen(csv_url)
+     csv = response.read()
+     csv_str =  str(csv)
+     lines = csv_str.split("\\n")
+     dest_url = r'yahoo.csv'
+     fx = open(dest_url, 'w')
+     for line in lines:
+          fx.write(line + "\n")
+     fx.close()
+download_stock_data(yahoo_url)
