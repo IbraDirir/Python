dbd5f91765d993cf99f130a534025a810225fc14
IbraDirir <khaliildirir@gmail.com>
2015-09-27 05:25:20 +0300

    Unpacking arguments

--- learn.py
+++ learn.py
@@ -1,9 +1,10 @@
-#flexible number of arguments
-def add_numbers(*add):
-    total =  0
-    for  a in add:
-        total += a
-    print(total)
-add_numbers(5)
-add_numbers(9,10)
-add_numbers(10,100,1000)
+#unpacking arguments
+def health_calculator(age, apples_ate, cigs_smoke):
+    answer = (100-age)+(apples_ate * 3.5) - (cigs_smoke*2)
+    print(answer)
+ibras_data = [21, 5, 0]
+
+mohas_data = [45, 0, 20]
+
+health_calculator(*mohas_data)
+health_calculator(*ibras_data)
