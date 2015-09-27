513c5dd79d1b46456fcf5cfeea041bbe937445db
IbraDirir <khaliildirir@gmail.com>
2015-09-27 22:43:55 +0300

    unpack tupes

--- learn.py
+++ learn.py
@@ -1,4 +1,6 @@
-date, item, price = ['December 20, 2015','Bread Gloves', '90']
-print(item)
-print(date)
-print(price)
+def drop_first_last(grades):
+    first, *middle, last = grades
+    avg = sum(middle) / len(middle)
+    print(avg)
+drop_first_last([90,87,90,21,30])
+drop_first_last([90,90,98,78,89])
