4d0d44aea0e3850f6c3d1fa5492af44f6e9949bd
IbraDirir <khaliildirir@gmail.com>
2015-09-27 19:48:36 +0300

    classes and objects

--- learn.py
+++ learn.py
@@ -1,13 +1,20 @@
-from urllib import request
-yahoo_url =  "http://www.canbike.org/information-technology/yahoo-finance-key-statistics-download-to-a-csv-file.html"
-def download_stock_data(csv_url):
-     response = request.urlopen(csv_url)
-     csv = response.read()
-     csv_str =  str(csv)
-     lines = csv_str.split("\\n")
-     dest_url = r'yahoo.csv'
-     fx = open(dest_url, 'w')
-     for line in lines:
-          fx.write(line + "\n")
-     fx.close()
-download_stock_data(yahoo_url)
+class Enemy:
+    life = 3
+    def attack(self):
+        print('ouch!')
+        self.life -= 1
+
+    def checklife(self):
+     if self.life <= 0:
+        print('dead!')
+     else:
+        print(str(self.life) + ' life left')
+
+enemy1 = Enemy()
+enemy2 = Enemy()
+enemy1.attack()
+enemy1.attack()
+enemy1.checklife()
+enemy2.attack()
+enemy2.checklife()
+
