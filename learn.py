361bf62817e05658d93788a969c5fbf414cfad18
IbraDirir <khaliildirir@gmail.com>
2015-09-27 21:34:51 +0300

    threading

--- learn.py
+++ learn.py
@@ -1,12 +1,11 @@
-class Mario():
-    def move(self):
-        print('i am moving')
+import threading
 
-class Shroom():
-    def eat_shroom(self):
-        print('i am  eating shroom')
-class BigMario(Mario, Shroom):
-    pass
-bm = BigMario()
-bm.move()
-bm.eat_shroom()
+class IbraMessenger(threading.Thread):
+    def run(self):
+        for _ in range(10):
+            print(threading.currentThread().getName())
+
+x = IbraMessenger(name = 'send out messages')
+y = IbraMessenger(name = 'send out messages')
+x.start()
+y.start()
