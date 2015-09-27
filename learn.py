15ab409cb167c31a4bb52e38d0d4a2d76c8d0f91
IbraDirir <khaliildirir@gmail.com>
2015-09-27 22:51:54 +0300

    ziptuples

--- learn.py
+++ learn.py
@@ -1,6 +1,8 @@
-def drop_first_last(grades):
-    first, *middle, last = grades
-    avg = sum(middle) / len(middle)
-    print(avg)
-drop_first_last([90,87,90,21,30])
-drop_first_last([90,90,98,78,89])
+#zip
+first = ['Ibra', 'Moha', 'Ali' ]
+last = ['Mahad', 'Daud', 'Adan']
+
+names = zip(first,last)
+
+for a,b in names:
+    print(a,b)
