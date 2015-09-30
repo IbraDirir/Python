e4f7156a63e529471f5d7ec673d061e58867b52e
IbraDirir <khaliildirir@gmail.com>
2015-09-30 20:08:50 +0300

    function taking a list as a paramater

--- learn.py
+++ learn.py
@@ -1,7 +1,7 @@
-for n in range(2, 10):
-    for x in range(2, n):
-        if n % x == 0:
-            print(n, 'equals', x, '*', n//x)
-            break
-    else:
-        print('n is a prime number')
+def sum(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+n = [1, 2, 5, 10, 13]
+print(sum(n))
