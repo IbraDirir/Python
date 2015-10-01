d23a0dd46548d16ffbb6e290ffd65885f8bc74fe
IbraDirir <khaliildirir@gmail.com>
2015-10-01 22:34:20 +0300

    in, not  in adding strings

--- learn.py
+++ learn.py
@@ -1,7 +1,13 @@
-def fib(n):
-    a, b = 0, 1
-    while a < n:
-        print(a, end ='')
-        a, b = b, a+b
-        print()
-fib(2000)
+magic_number = 1500
+cupsOfFlour = 3
+magic_number = 1500
+eggs_per_cake = 3
+number_of_cakes = 10
+total_eggs = eggs_per_cake * number_of_cakes
+print(total_eggs)
+name = 'ibrahim'
+print('hi '+ name)
+
+print('z' not in 'hello')
+if 'banana' not in 'bananarama':
+    print("i miss 80's")
