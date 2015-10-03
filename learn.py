f238008e675133728f77e3421fbf3f9596823210
IbraDirir <khaliildirir@gmail.com>
2015-10-03 11:41:12 +0300

    calling a function

--- learn.py
+++ learn.py
@@ -1,13 +1,3 @@
-names = ['Ali', 'Eli',  'rick',  'zelda']
-for name in names:
-    if name[0] in 'AEIOU':
-        print(name + ' starts with vowel')
-
-for name in names:
-    counter = 1
-    counter = counter + 1
-    print(counter)
-sentence = ""
-for  name  in names:
-    sentence = sentence + " " + name
-    print(name)
+def function(boy, girl):
+    print(boy, girl)
+function("igaar",  "gabar")
