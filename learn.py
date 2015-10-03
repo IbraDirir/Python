0d739b9f906004b548eafb1414467bf9e6a8f44f
IbraDirir <khaliildirir@gmail.com>
2015-10-03 12:16:37 +0300

    passed ssstring into its function

--- learn.py
+++ learn.py
@@ -1,3 +1,9 @@
-def function(boy, girl):
-    print(boy, girl)
-function("igaar",  "gabar")
+# Function definition is here
+def printme( str ):
+   "This prints a passed string into this function"
+   print(str)
+   return;
+my_story = 'I will tell you later'
+# Now you can call printme function
+printme('do you want to hear my story, '
++ my_story)
