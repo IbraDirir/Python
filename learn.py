09248879ab42ae09ebce6d130d1df72566a48ac9
IbraDirir <khaliildirir@gmail.com>
2015-10-04 19:13:37 +0300

    print random numbers

--- learn.py
+++ learn.py
@@ -1,11 +1,4 @@
-#conditional  statements
-name = 'boy'
-BoyName = 'ibra'
-if name == 'girl':
-    if BoyName =='ali':
-        print('ibra  is amazing guy')
-    else:
-        print('no name ibra')
-
-else:
-    print('no boys')
+import random
+for  i in range(10):
+    x = random.random()
+    print(x)
