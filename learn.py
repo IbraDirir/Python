ce2d5d4645553b1c78f197af7f26bd9e5815cbe0
IbraDirir <khaliildirir@gmail.com>
2015-10-04 12:20:28 +0300

    else statement

--- learn.py
+++ learn.py
@@ -1,4 +1,6 @@
 #conditional  statements
 n = int(input("Number?"))
 if n<0:
-    print("absolute value of", n, "is", n)
+    print("absolute value of", n, "is", -n)
+else:
+    print("put number less than zero, please")
