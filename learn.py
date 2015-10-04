02dec98af063d566bc7a7ade54d4571720c6825b
IbraDirir <khaliildirir@gmail.com>
2015-10-04 08:43:23 +0300

    break statement in range

--- learn.py
+++ learn.py
@@ -1,3 +1,7 @@
-a = ['marry', 'isak', 'ali', 'ismasha']
-for i in range(len(a)):
-   print(i, a[i])
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        print(n,  'is a prime number')
