c8ef0349bd1e9f5166ae24f6e321810cf7c05f64
IbraDirir <khaliildirir@gmail.com>
2015-10-04 11:57:59 +0300

    extinding to a new list

--- learn.py
+++ learn.py
@@ -5,5 +5,10 @@ pow(4,5)
 print(max(mylist))
 import math
 print(math.sqrt(25))
+#defining variable for a module
 squareRoot = math.sqrt
 print(squareRoot(9))
+#addibg lists
+names = ['ibra', 'john','kate','frank']
+print(names)
+print(mylist.extend(names))
