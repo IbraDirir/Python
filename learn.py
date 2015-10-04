71856c71696857c84b8651208a6e31a1964498a8
IbraDirir <khaliildirir@gmail.com>
2015-10-04 12:17:39 +0300

    input in if statement

--- learn.py
+++ learn.py
@@ -1,19 +1,4 @@
-mylist = [0,1,2,3,4,5,6,7,8,9]
-print(mylist)
-print(mylist[1:7])
-pow(4,5)
-print(max(mylist))
-import math
-print(math.sqrt(25))
-#defining variable for a module
-squareRoot = math.sqrt
-print(squareRoot(9))
-#addibg lists
-names = ['ibra', 'john','kate','frank']
-#remoing from a list
-names.remove('frank')
-print(names)
-print(mylist.extend(names))
-print(mylist,names)
-print(len(names))
-print(max(names))
+#conditional  statements
+n = int(input("Number?"))
+if n<0:
+    print("absolute value of", n, "is", n)
