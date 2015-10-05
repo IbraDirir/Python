1d95038e541ef756a1e43ab6f4d95dbfce7d671e
IbraDirir <khaliildirir@gmail.com>
2015-10-05 08:11:33 +0300

    #practical example of while loop

--- learn.py
+++ learn.py
@@ -1,4 +1,11 @@
+#practical example of while loop
 a = 1
-while a < 5:
-    print(a)
-    a += 1
+s = 0
+print("Enter Numbers to add to the sum. ")
+print('Enter 0 to quit. ')
+while a != 0:
+    print('currents sum: ', s)
+    a = float(input('Numbers?'))
+    a = float(a)
+    s += a #s = s + a
+print('Total sum = ',s)
