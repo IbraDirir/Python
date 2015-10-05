9be2fa928024ded9e7a74cbecc99aed80e10e324
IbraDirir <khaliildirir@gmail.com>
2015-10-05 20:08:13 +0300

    inheritance and subclasses

--- learn.py
+++ learn.py
@@ -21,7 +21,10 @@ studentScores("mark", 89, 90, 100, 100, 78)
 class Person:
     #initiliazing a class constructor
     def __init__(self):
+
         print("no need for explicit calling")
+    def __del__(self):
+        print("Our class  instance is distroyed")
     def setFullName(self,FirstName, LastName):
         self.FirstName = FirstName
         self.LastName = LastName
@@ -30,6 +33,26 @@ class Person:
 personName = Person()
 personName.setFullName("Ibrahim", "Dirir")
 personName.printFullName()
+personName.__del__()
 
 
+#inheritance
+#inheritance allows us to define a
+#class in terms of anather
+#class, which makes it easier
+#to  create and  maintain  an application
+
+class Parent:
+    value1 = "this is value 1"
+    value2 = "this is value 2"
+class Child(Parent):
+    pass
+parent_=Parent()
+print(parent_.value1)
+
+print(parent_.value2)
+child_=Child()
+print(child_.value1)
+
+print(child_.value2)
 
