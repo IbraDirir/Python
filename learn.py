39bcc72e31697e106fc6029278cbbe4a3361c1ad
IbraDirir <khaliildirir@gmail.com>
2015-10-05 15:52:46 +0300

    student's grades more subjects

--- learn.py
+++ learn.py
@@ -8,4 +8,10 @@ studentScore("Ibra", 99)
 def studentScore(name="tom", score=0):
     print(name,"got", score, "points")
 
-studentScore("mark",100) #you can overwrite default paramaters
+studentScore("mark",100)#you can overwrite default paramaters
+
+#passing multiple values to one paramater
+#wild cards
+def studentScores(name,*score):
+    print(name,"scores",score, "Marks")
+studentScores("mark", 89, 90, 100, 100, 78)
