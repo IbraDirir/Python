1345c6213f2a62e69c72121a6941b93a100dc02b
IbraDirir <khaliildirir@gmail.com>
2015-10-05 08:18:12 +0300

    difference between for and while  loop

--- learn.py
+++ learn.py
@@ -2,3 +2,5 @@
 b = [1,2,4,5,6,7,8,9]
 for num in b:
     print(num)
+#difference between for and while  looop is that while loop waits
+#the condition to  be true  while for loop  there is no condition
