f590c76e5d16b003d6b32005964104fd0724f238
IbraDirir <khaliildirir@gmail.com>
2015-10-05 08:16:09 +0300

    for loop

--- learn.py
+++ learn.py
@@ -1,11 +1,4 @@
-#practical example of while loop
-a = 1
-s = 0
-print("Enter Numbers to add to the sum. ")
-print('Enter 0 to quit. ')
-while a != 0:
-    print('currents sum: ', s)
-    a = float(input('Numbers?'))
-    a = float(a)
-    s += a #s = s + a
-print('Total sum = ',s)
+#for loop
+b = [1,2,4,5,6,7,8,9]
+for num in b:
+    print(num)
