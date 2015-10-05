dd30acf1ee0c20f7c9adf0968c25f855056c00f3
IbraDirir <khaliildirir@gmail.com>
2015-10-05 15:27:29 +0300

    student scored

--- learn.py
+++ learn.py
@@ -1,11 +1,4 @@
-#A function is a group of statement that together performs specific  task
-#again and again whithout  writing the code  again and again
-#functions make the code readable and maintanable
-#def funtionname(arg1,arg2...):
-#statement1
-#statement2
-def hello(name):
-    print('hello ', name)
-hello('Ibrahim Dirir')
-hello('Ali')
-hello('Ian')
+#default paramaters and Multiple Arguments
+def studentScore(name, score):
+    print(name, "Scored", score,"Marks")
+studentScore("Ibra", 99)
