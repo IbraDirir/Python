56e37bd6f6ae2cf48c44a939ef6e50cbc8484a1e
IbraDirir <khaliildirir@gmail.com>
2015-10-05 15:39:58 +0300

    default paramaters

--- learn.py
+++ learn.py
@@ -1,4 +1,10 @@
 #default paramaters and Multiple Arguments
+#student score
 def studentScore(name, score):
     print(name, "Scored", score,"Marks")
 studentScore("Ibra", 99)
+#default paramater
+
+def studentScore(name="tom", score=0):
+    print(name,"got", score, "points")
+studentScore()
