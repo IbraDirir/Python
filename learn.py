1b1c177e94a1e0b3191b37533a9ec39417f5d04e
IbraDirir <khaliildirir@gmail.com>
2015-10-07 03:37:49 +0300

    function inside a list

--- learn.py
+++ learn.py
@@ -6,5 +6,12 @@ fr = open("sample.txt", "r")
 text = fr.read()
 print(text)
 fr.close()
+#function inside lists
+def list_function(x):
 
+    x[1] = x[1] + 3
+    return x
+
+n = [3, 5, 7]
+print(list_function(n))
 
