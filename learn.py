9bbfdc251de8cc957e1165a964e34469af26f590
IbraDirir <khaliildirir@gmail.com>
2015-10-07 19:52:19 +0300

    battleship game

--- learn.py
+++ learn.py
@@ -1,17 +1,5 @@
-fw = open("sample.txt", "w")
-fw.write("writing stuff in my file\n")
-fw.write("cool")
-fw.close()
-fr = open("sample.txt", "r")
-text = fr.read()
-print(text)
-fr.close()
-#function inside lists
-def list_function(x):
-
-    x[1] = x[1] + 3
-    return x
-
-n = [3, 5, 7]
-print(list_function(n))
-
+#battleship game
+board = []
+for num in range(5):
+    board.append(["O"] * 5)
+    print(board)
