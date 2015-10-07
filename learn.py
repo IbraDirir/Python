bcba7a1ea98183e54135d30ec2e3f4d9822460d3
IbraDirir <khaliildirir@gmail.com>
2015-10-07 20:31:34 +0300

    import randint

--- learn.py
+++ learn.py
@@ -7,3 +7,13 @@ def print_board(board):
     for item in board:
         print(" ".join(item)) #print without  commas, or join the list
 print_board(board)
+#import function(randint)
+
+from random import randint
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+random_row(board)
+random_col(board)
