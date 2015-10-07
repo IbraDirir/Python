b3667135511653888b0377a2bf4a64e25b251f71
IbraDirir <khaliildirir@gmail.com>
2015-10-07 03:36:04 +0300

    reading from a file and writing to a file

--- learn.py
+++ learn.py
@@ -1,58 +1,10 @@
-#default paramaters and Multiple Arguments
-#student score
-def studentScore(name, score):
-    print(name, "Scored", score,"Marks")
-studentScore("Ibra", 99)
-#default paramater
+fw = open("sample.txt", "w")
+fw.write("writing stuff in my file\n")
+fw.write("cool")
+fw.close()
+fr = open("sample.txt", "r")
+text = fr.read()
+print(text)
+fr.close()
 
-def studentScore(name="tom", score=0):
-    print(name,"got", score, "points")
-
-studentScore("mark",100)#you can overwrite default paramaters
-
-#passing multiple values to one paramater
-#wild cards
-def studentScores(name,*score):
-    print(name,"scores",score, "Marks")
-studentScores("mark", 89, 90, 100, 100, 78)
-
-
-#Classes
-class Person:
-    #initiliazing a class constructor
-    def __init__(self):
-
-        print("no need for explicit calling")
-    def __del__(self):
-        print("Our class  instance is distroyed")
-    def setFullName(self,FirstName, LastName):
-        self.FirstName = FirstName
-        self.LastName = LastName
-    def printFullName(self):
-        print(self.FirstName, " ", self.LastName)
-personName = Person()
-personName.setFullName("Ibrahim", "Dirir")
-personName.printFullName()
-personName.__del__()
-
-
-#inheritance
-#inheritance allows us to define a
-#class in terms of anather
-#class, which makes it easier
-#to  create and  maintain  an application
-
-class Parent:
-    value1 = "this is value 1"
-    value2 = "this is value 2"
-class Child(Parent):
-    pass
-parent_=Parent()
-print(parent_.value1)
-
-print(parent_.value2)
-child_=Child()
-print(child_.value1)
-
-print(child_.value2)
 
