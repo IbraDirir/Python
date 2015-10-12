86c4fd23259989d4af70e3a02922c14bf0c7e90f
IbraDirir <khaliildirir@gmail.com>
2015-10-12 14:37:00 +0300

    lumberjack

---
+++ lumberjack.py
@@ -0,0 +1,5 @@
+name = input("what's your name? ")
+if name == "Ibra":
+    print(name+ "is a lumberjack and he is fine!")
+else:
+    print(name + " sleeps all  day and " +name + " works all  day! ")
