4a9dd0bacd3fae18c513fe15a465211d7ac34d0a
IbraDirir <khaliildirir@gmail.com>
2015-10-12 20:07:05 +0300

    hi, I am ibra  with lists

---
+++ learn.py
@@ -0,0 +1,6 @@
+full_name = "Ibra Rawlinson";
+name_list = full_name.split();
+greeting_list = "Hi, I'm Treehouse".split();
+greeting_list[2] = name_list[0];
+greeting = " ".join(greeting_list)
+print(greeting)
