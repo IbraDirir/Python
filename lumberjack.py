44ffd2aaf28c379784e084cc427842422dd30862
IbraDirir <khaliildirir@gmail.com>
2015-10-12 14:46:56 +0300

    format instead of concancenate

--- lumberjack.py
+++ lumberjack.py
@@ -1,5 +1,5 @@
 name = input("what's your name? ")
 if name == "Ibra":
-    print(name+ "is a lumberjack and he is fine!")
+    print("{} is a lumberjack and he is fine!".format(name))
 else:
-    print(name + " sleeps all  day and " +name + " works all  day! ")
+    print("{} sleeps all  day and {}  works all  day!".format(name, name))
