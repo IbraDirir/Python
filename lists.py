da95287d9016ad9cdf8524f0599e82534a16eaa4
IbraDirir <khaliildirir@gmail.com>
2015-10-12 19:29:34 +0300

    shopping in the supermarket

--- lists.py
+++ lists.py
@@ -5,3 +5,8 @@ mylist[3]= 'b'
 print(mylist)
 mlist = list('hello')
 print(mlist)
+mysentence = "I am great and awsome!"
+sentencelist=mysentence.split()
+
+' '.join(sentencelist)
+print(sentencelist)
