7727367b294155a16d0b1f1613a129d571988d60
IbraDirir <khaliildirir@gmail.com>
2015-10-12 15:39:50 +0300

    int string multiply

--- lumberjack.py
+++
@@ -1,5 +0,0 @@
-name = input("what's your name? ")
-if name == "Ibra":
-    print("{} is a lumberjack and he is fine!".format(name))
-else:
-    print("{} sleeps all  day and {}  works all  day!".format(name, name))
---
+++ string_multiply.py
@@ -0,0 +1,3 @@
+input_string = input("what  do like to eat")
+input_int = input("how many times do you eat in a day?")
+print(input_string * int(input_int))
